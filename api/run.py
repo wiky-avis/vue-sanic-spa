11a1df6043d1d904beab407bb025edf61b201558
wiky-avis <peta-holding@yandex.ru>
2021-08-23 17:28:37 +0300

    api

---
+++ api/run.py
@@ -0,0 +1,21 @@
+import os
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+
+@app.route("/")
+async def test(request):
+    return json({"hello": "world"})
+
+if __name__ == "__main__":
+    debug_mode = os.getenv('API_MODE', '') == 'dev'
+
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=debug_mode,
+        access_log=debug_mode,
+        auto_reload=debug_mode
+    )
